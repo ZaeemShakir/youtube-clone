5520986b5d789973b75ccf9be95ded02466ea4ea
Zaeem Shakir <shakirzaeem@gmail.com>
2023-04-24 21:13:43 -0700

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
+import Videos from "./Videos";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sc: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          copyright 2023 Zaeem Shakir
+        </Typography>
+      </Box>
+      <Box p={2} sx={{overflow:'auto' ,height:'90vh',flex:2}}>
+        <Typography
+          varient="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New<span style={{ color: "#f31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={[]}/>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,11 @@
+import { useState,useEffect } from "react"
+
+const Videos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Videos
